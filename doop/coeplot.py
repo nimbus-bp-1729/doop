1224e5b01000544ae78d4c1ccc79a123d1d53f55
Kevin <walchko@users.noreply.github.com>
2020-06-06 10:07:24 -0400

    Update coeplot.py

--- doop/coeplot.py
+++ doop/coeplot.py
@@ -1,3 +1,34 @@
 
 from math import pi, sqrt, sin, cos 
 #from matplotlib import pyplot as plt
+
+def perifocal(coe, f):
+    """
+    Given the COE for an orbit and the angle (f) from perisapsis (in
+    radians), then this returns the (p,q) cartesian locationl
+    
+    https://www.sciencedirect.com/topics/engineering/perifocal-frame
+    """
+    p = coe.a * (1 - coe.e ** 2)
+    pos = np.array([cos(f), sin(f)]) * p / (1 + coe.e * cos(f))
+    return pos
+  
+def orbit(coe):
+    """
+    Produces the (x,y) or (p,q) locations with in the perifocal plan (always 2D)
+    of the orbit. Allows a simple 2D plot in jupyter notebook with: 
+    
+        r = orbit(coe)
+        plt.plot(r[:,0], r[:,1],'--r')
+        plt.grid(True);
+    
+    coe: Classical Orbital Elements
+    return: numpy([(x,y),(x,y), ...]) which has a shape of (360,2) 
+    
+    https://www.sciencedirect.com/topics/engineering/perifocal-frame
+    """
+    oorb = np.zeros((360,2))
+    for i,f in enumerate(range(360)):
+        oorb[i,:] = perifocal2(sat.coe, f*pi/180)
+
+    return orb
