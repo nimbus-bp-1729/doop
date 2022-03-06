8471489522153629dfabd30a46cdbfe25ace6362
walchko <walchko@users.noreply.github.com>
2022-03-05 18:17:16 -0700

    update python

--- doop/__init__.py
+++ doop/__init__.py
@@ -1,7 +1,5 @@
-try:
-    from importlib.metadata import version # type: ignore
-except ImportError:
-    from importlib_metadata import version # type: ignore
+
+from importlib.metadata import version # type: ignore
 
 
 __author__ = 'Kevin Walchko'
